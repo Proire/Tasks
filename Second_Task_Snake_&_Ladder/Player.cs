384d8556d698753b32b07200add4f45baec85373
Proire <prasadahire4501@gmail.com>
2024-04-20 15:11:27 +0530

    [Prasad] Add : Game Intialization Completed with One Player and Tested Successfully

---
+++ Second_Task_Snake_&_Ladder/Player.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Second_Task_Snake___Ladder
+{
+    internal class Player(string name, int playerPosition)
+    {
+        public string Name { get; set; } = name;
+
+        public int PlayerPosition { get; set; } = playerPosition;
+    }
+}
+
+
