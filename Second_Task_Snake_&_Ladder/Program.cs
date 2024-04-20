5219dfe6a0df3a14b712652fc54618c6ede84924
Proire <prasadahire4501@gmail.com>
2024-04-20 15:06:06 +0530

    [Prasad] Add : Snake and Ladder Application Development Started

---
+++ Second_Task_Snake_&_Ladder/Program.cs
@@ -0,0 +1,10 @@
+﻿namespace Second_Task_Snake___Ladder
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to the Snake and Ladder Game Simulator!");
+        }
+    }
+}
