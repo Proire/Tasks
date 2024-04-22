6116e02cf814116f55701215330cbaa56a9a5dcf
Proire <prasadahire4501@gmail.com>
2024-04-22 12:55:54 +0530

    [Prasad] Add : Successfully Completed the Whole Applications with all possible validations and comments

--- Second_Task_Snake_&_Ladder/Player.cs
+++ Second_Task_Snake_&_Ladder/Player.cs
@@ -10,8 +10,8 @@ namespace Second_Task_Snake___Ladder
 
     internal class SnakeAndLadder
     {
+        // instances members representating state of Board
         public  int[] Board { get; set; } = new int[101];
-
         public readonly Player playerOne;
         public readonly Player playerTwo;
         public Player CurrentPlayer;
@@ -29,6 +29,7 @@ namespace Second_Task_Snake___Ladder
 
         public void InitializeBoard()
         {
+            // Define the positions of Board blocks
             for (int i = 1; i <= 100; i++)
             {
 
@@ -48,12 +49,14 @@ namespace Second_Task_Snake___Ladder
             print();
         }
 
+        // method to roll the dice
         public int RollDie()
         {
             Count++;
             return random.Next(1, 7);
         }
 
+        // method to ask for roll the dice and ask for new player position and print it
         public void MovePlayer(Player player)
         {
             int roll = RollDie();
@@ -62,35 +65,51 @@ namespace Second_Task_Snake___Ladder
             Console.WriteLine($"Player {player.Name} is now at square {player.PlayerPosition}.\n\n");
         }
 
+        // method to actually move the player
         private int MovePlayerPosition(int currentPosition, int roll)
         {
             // stopping Player from going above 100 
             if (currentPosition + roll > 100)
                 return currentPosition;
+
+            // new position from currentposition of player and rolled Dice
             int newPosition = currentPosition + roll;
+
+            // if player is on ladder position get another chance to roll the dice
             if (newPosition == 6 || newPosition == 77)
                 RollAgain = true;
+            else
+                RollAgain = false;  
+
+            // Declaring newSquare
             int newSquare;
+
+            // adding or subtracting position when ladder or snake position reached
             if (newPosition == 6 || newPosition == 23 || newPosition == 45 || newPosition == 61 || newPosition == 77 || newPosition==98 || newPosition==65)
                 newSquare = newPosition + Board[newPosition];
             else
                 newSquare = newPosition;
+
+            // returning newsquare or new position only when less than 100 otherwise sameposition
             return newSquare > 100 ? currentPosition : newSquare;
         }
 
         public void print()
         {
 
-            int alt = 0; // to switch between the alternate nature of the board
+            int alt = 0; // to switch between the alternate nature of the board or rows
             int iterLR = 101; // iterator to print from left to right
             int iterRL = 80;  // iterator to print from right to left
-            int val = 100;
+            int val = 100;    // starting position top of the array 
+
+            // creating space for better formatting
+            Console.WriteLine();
 
             while (val-- > 0)
             {
-                if (alt == 0)
+                if (alt == 0)  
                 {
-                    iterLR--;
+                    iterLR--; // print from left to right in decreasing sequence
                     if (iterLR == playerOne.PlayerPosition)
                     {
                         Console.Write(playerOne.Name.Substring(0,3) + "    ");
@@ -107,6 +126,7 @@ namespace Second_Task_Snake___Ladder
                             Console.Write(Board[iterLR].ToString("000") + "    ");
                     }
 
+                    // check for the last digit is one
                     if (iterLR % 10 == 1)
                     {
                         Console.WriteLine("\n");
@@ -116,7 +136,7 @@ namespace Second_Task_Snake___Ladder
                 }
                 else
                 {
-                    iterRL++;
+                    iterRL++;// print in increaseing sequence from right to left
                     if (iterRL == playerOne.PlayerPosition)
                     {
                         Console.Write(playerOne.Name.Substring(0, 3) + "    ");
@@ -133,6 +153,7 @@ namespace Second_Task_Snake___Ladder
                             Console.Write(Board[iterRL].ToString("000") + "    ");
                     }
 
+                    // check for last digit is zero
                     if (iterRL % 10 == 0)
                     {
                         Console.WriteLine("\n");
@@ -140,7 +161,7 @@ namespace Second_Task_Snake___Ladder
                         iterRL -= 30;
                     }
                 }
-                if (iterRL == 10)
+                if (iterRL == 10) 
                     break;
             }
             Console.WriteLine();
