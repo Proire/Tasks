3e30a00ea457b0038dafc0822a9a96be64bf03db
Proire <prasadahire4501@gmail.com>
2024-04-26 16:15:57 +0530

    [Prasad] Add : Completed and tested succesfully logic of Bredth first search

--- GraphImplementation/GraphDemo.cs
+++ GraphImplementation/GraphDemo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
+using System.Runtime.InteropServices;
 using System.Text;
 using System.Threading.Tasks;
 
@@ -9,22 +10,50 @@ namespace GraphImplementation
     internal class GraphDemo
     {
         private int v;
+        private int[]? visited;
         private int[,]? graph;
 
         public void CreateGraph(int nodes)
         {
             v = nodes;
+            visited = new int[v];  
             graph = new int[v,v];
             for (int i = 0; i < v; i++)
             {
                 for (int j = 0; j < v; j++)
                 {
-                    Console.WriteLine($"Enter the value for v{i} to v{j} (999) for infinity");
+                    Console.WriteLine($"Enter 1(edge) or 0(not edge) for v{i} to v{j}");
                     _ = int.TryParse(Console.ReadLine(), out graph[i, j]);
                 }
             }
         }
 
+        public void BFS(int source)
+        {
+            int[] arr = new int[v];   
+            int front = 0, rear = -1;
+            visited[source] = 1;
+            int count = 0;
+            arr[++rear] = source; 
+            while (front <= rear)
+            {
+                int element = arr[front++];
+                count++;
+                if (count != v)
+                    Console.Write($"v{element} - ");
+                else
+                    Console.WriteLine($"v{element}");
+                for(int i=0; i < v; i++)
+                {
+                    if (graph[element,i] == 1 && visited[i] != 1)
+                    {
+                        visited[i] = 1;
+                        arr[++rear] = i;
+                    }
+                }
+
+            }
+        }
         public void PrintGraph()
         {
             if (graph != null)
@@ -39,6 +68,7 @@ namespace GraphImplementation
                     Console.WriteLine();
                 }
             }
+            Console.WriteLine() ;
         }
     }
 }
--- GraphImplementation/Program.cs
+++ GraphImplementation/Program.cs
@@ -13,12 +13,17 @@
             int nodes;
             Console.Write("Enter the size of the graph : ");
             _ = int.TryParse(Console.ReadLine(), out nodes);
-        
 
             graph.CreateGraph(nodes);
 
             // print the graph 
             graph.PrintGraph();
+
+            // Breadth First Search
+            int source;
+            Console.Write("Enter the Starting Vertice : ");
+            _ = int.TryParse(Console.ReadLine(), out source);
+            graph.BFS(source);
 
         }
     }
