bf2911370ec388daf190440ffb62b234bfbd608b
Proire <prasadahire4501@gmail.com>
2024-04-26 14:42:11 +0530

    [Prasad] Add : New Branch Created to work for BFS

---
+++ GraphImplementation/GraphDemo.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GraphImplementation
+{
+    internal class GraphDemo
+    {
+        private int v;
+        private int[,]? graph;
+
+        public void CreateGraph(int nodes)
+        {
+            v = nodes;
+            graph = new int[v,v];
+            for (int i = 0; i < v; i++)
+            {
+                for (int j = 0; j < v; j++)
+                {
+                    Console.WriteLine($"Enter the value for v{i} to v{j} (999) for infinity");
+                    _ = int.TryParse(Console.ReadLine(), out graph[i, j]);
+                }
+            }
+        }
+
+        public void PrintGraph()
+        {
+            if (graph != null)
+            {
+                Console.WriteLine("\nGraph Representation : \n");
+                for (int i = 0; i < v; i++)
+                {
+                    for (int j = 0; j < v; j++)
+                    {
+                        Console.Write($"{graph[i, j]} \t");
+                    }
+                    Console.WriteLine();
+                }
+            }
+        }
+    }
+}
---
+++ GraphImplementation/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace GraphImplementation
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome the Graph Implementation!");
+            
+            // Instantiate Graph 
+            GraphDemo graph = new GraphDemo();
+
+            // size of Graph or number of nodes input from User
+            int nodes;
+            Console.Write("Enter the size of the graph : ");
+            _ = int.TryParse(Console.ReadLine(), out nodes);
+        
+
+            graph.CreateGraph(nodes);
+
+            // print the graph 
+            graph.PrintGraph();
+
+        }
+    }
+}
